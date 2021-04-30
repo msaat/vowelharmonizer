816275c84e169ae5ecc14135983680fb46db6df3
maria saatgian <>
2021-04-30 03:31:49 +0000

    added more functions for suffixes in twoVowel

--- Main.java
+++ Main.java
@@ -12,9 +12,10 @@ class Main {
     String inputtedWord = myObj.nextLine();  
     //System.out.println(inputtedWord);
     Scanner myOtherObj = new Scanner(System.in);
-    System.out.println("type name of function from the following list:\nplural, without, ");
+    System.out.println("type name of function from the following list:\nplural, without, from, at, with, to");
    String choice = myOtherObj.nextLine();   
     String v = vowelSearch(inputtedWord);
+    String lastLet = lastLetter(inputtedWord);
     if (choice.equals("plural")){
       String y = twoVowel.plural(v);
       String finalPlural = inputtedWord.concat(y);
@@ -25,6 +26,27 @@ class Main {
       String finalWithout = inputtedWord.concat(y);
       System.out.println(finalWithout);
     }
+    if (choice.equals("from")){
+      String y = twoVowel.from(v);
+      String finalFrom = inputtedWord.concat(y);
+      System.out.println(finalFrom);
+    }
+    if (choice.equals("at")){
+      String y = twoVowel.at(v);
+      String finalAt = inputtedWord.concat(y);
+      System.out.println(finalAt);
+    }
+    if (choice.equals("with")){
+      String y = twoVowel.with(v);
+      String finalWith = inputtedWord.concat(y);
+      System.out.println(finalWith);
+    }
+    if (choice.equals("to")){
+      String y = twoVowel.to(v,lastLet);
+      String finalTo = inputtedWord.concat(y);
+      System.out.println(finalTo);
+    }
+
 
 
   }
@@ -42,10 +64,14 @@ class Main {
        return lettertwo;
      }
        
-      
-     
-     
   }
+
+  public static String lastLetter(String m){
+    String letter = m.substring(m.length()-1,m.length());
+    return letter;
+  }
+       
+  
 
 
 }
--- twoVowel.java
+++ twoVowel.java
@@ -19,8 +19,38 @@ public static String plural(String n){
   return ending;
 }
 
+public static String from(String n){
+  String changer = changerFinder(n);
+  
+  String ending = "d"+changer+"n";
+  return ending;
+}
 
+public static String at(String n){
+  String changer = changerFinder(n);
+  
+  String ending = "d"+changer;
+  return ending;
+}
 
+public static String with(String n){
+  String changer = changerFinder(n);
+  
+  String ending = "l"+changer;
+  return ending;
+}
+
+public static String to(String n, String m){
+  String changer = changerFinder(n);
+  String ending = "";
+  if (n.equals(m)){ 
+  ending = "y"+changer;
+  }
+  else {
+  ending = changer;
+  }
+  return ending;
+}
 
 
 
